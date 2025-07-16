26e9958e7ae0ad63cd902c378888fe9a87d0dcdd
Sergio Méndez <sergioarm.gpl@gmail.com>
2025-07-16 12:07:19 -0600

    feat: updated intro link
    Signed-off-by: Sergio Méndez <sergioarm.gpl@gmail.com>

--- src/pages/index.js
+++ src/pages/index.js
@@ -19,7 +19,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/intro">
+            to="/docs/introduction/intro">
             Start using KubeMaya Now!
           </Link>
         </div>
